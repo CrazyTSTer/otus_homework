6b72efd3f93eb6c6faa68a8edf93506246b75078
Gribkov Ivan <igribkov@corp.badoo.com>
2020-10-08 12:42:49 +0300

    adding comments

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -13,14 +13,15 @@ type freq struct {
 func Top10(input string) []string {
 	result := []freq{}
 
-	input = strings.Replace(input, "\n", " ", -1) //Convert NEW_LINE to SPACE
-	input = strings.Replace(input, "\t", " ", -1) //Convert TAB to SPACE
+	input = strings.Replace(input, "\n", " ", -1) // Convert NEW_LINE to SPACE
+	input = strings.Replace(input, "\t", " ", -1) // Convert TAB to SPACE
 
 	regexp := regexp.MustCompile(" +")
-	sliceToAnalize := regexp.Split(input, -1) //Convert spaces sequence to one SPACE
+	sliceToAnalize := regexp.Split(input, -1) // Convert spaces sequence to one SPACE
 
-	sort.Slice(sliceToAnalize, func(i, j int) bool { return sliceToAnalize[i] < sliceToAnalize[j] })
+	sort.Slice(sliceToAnalize, func(i, j int) bool { return sliceToAnalize[i] < sliceToAnalize[j] }) // Sort slice to set correct words sequence
 
+	// Get counts of same words
 	wordCount := 1
 	for i := 1; i < len(sliceToAnalize); i++ {
 		if sliceToAnalize[i-1] == sliceToAnalize[i] {
@@ -31,8 +32,11 @@ func Top10(input string) []string {
 		}
 	}
 	result = append(result, freq{sliceToAnalize[len(sliceToAnalize)-1], wordCount})
+
+	// Sort slice to get frequently appearing words in the top
 	sort.Slice(result, func(i, j int) bool { return result[i].count > result[j].count })
 
+	// Get top 10
 	var ret []string
 	for i := 0; i < len(result) && i < 10; i++ {
 		if result[i].str == "" {
