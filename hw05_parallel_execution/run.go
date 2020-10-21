a2d6a420c7dce7e94675c99d15c1fd735b3f0afb
Gribkov Ivan <igribkov@corp.badoo.com>
2020-10-21 23:41:58 +0300

    finished hw05 with Aleksey's Bakin help

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05_parallel_execution //nolint:golint,stylecheck
 
 import (
 	"errors"
+	"sync"
+	"sync/atomic"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,49 @@ type Task func() error
 
 // Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
 func Run(tasks []Task, N int, M int) error {
-	// Place your code here
+	//Err counter
+	var errCount uint32
+
+	//Create task channel
+	taskCh := make(chan Task)
+
+	//Create wait group
+	wg := sync.WaitGroup{}
+	wg.Add(N)
+
+	//Run tasks in separate go routines
+	for i := 0; i < N; i++ {
+		go func() {
+			defer wg.Done()
+
+			//Execute tasks from channel
+			for task := range taskCh {
+				//Execute separate tasks, if it finished with error, increase error counter
+				if err := task(); err != nil {
+					atomic.AddUint32(&errCount, 1)
+				}
+			}
+		}()
+	}
+
+	//Push all tasks to channel
+	for _, task := range tasks {
+		//Stop pushing tasks if error counter >= M
+		if atomic.LoadUint32(&errCount) >= uint32(M) {
+			break
+		}
+		//Push task to channel
+		taskCh <- task
+	}
+
+	//Close channel
+	close(taskCh)
+	//Waite for all go-routines finished
+	wg.Wait()
+
+	//Check for error count and return an error if error counter >= M
+	if errCount >= uint32(M) {
+		return ErrErrorsLimitExceeded
+	}
 	return nil
 }
