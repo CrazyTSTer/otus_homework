3c7b09fc3370e0d06877e28ec0eeeb236dc4e4ab
Gribkov Ivan <igribkov@corp.badoo.com>
2020-10-22 00:23:27 +0300

    linters fix

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,26 +10,25 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-// Run starts tasks in N goroutines and stops its work when receiving M errors from tasks
-func Run(tasks []Task, N int, M int) error {
-	//Err counter
+func Run(tasks []Task, n int, m int) error {
+	// Err counter
 	var errCount uint32
 
-	//Create task channel
+	// Create task channel
 	taskCh := make(chan Task)
 
-	//Create wait group
+	// Create wait group
 	wg := sync.WaitGroup{}
-	wg.Add(N)
+	wg.Add(n)
 
-	//Run tasks in separate go routines
-	for i := 0; i < N; i++ {
+	// Run tasks in separate go routines
+	for i := 0; i < n; i++ {
 		go func() {
 			defer wg.Done()
 
-			//Execute tasks from channel
+			// Execute tasks from channel
 			for task := range taskCh {
-				//Execute separate tasks, if it finished with error, increase error counter
+				// Execute separate tasks, if it finished with error, increase error counter
 				if err := task(); err != nil {
 					atomic.AddUint32(&errCount, 1)
 				}
@@ -37,23 +36,23 @@ func Run(tasks []Task, N int, M int) error {
 		}()
 	}
 
-	//Push all tasks to channel
+	// Push all tasks to channel
 	for _, task := range tasks {
-		//Stop pushing tasks if error counter >= M
-		if atomic.LoadUint32(&errCount) >= uint32(M) {
+		// Stop pushing tasks if error counter >= M
+		if atomic.LoadUint32(&errCount) >= uint32(m) {
 			break
 		}
-		//Push task to channel
+		// Push task to channel
 		taskCh <- task
 	}
 
-	//Close channel
+	// Close channel
 	close(taskCh)
-	//Waite for all go-routines finished
+	// Waite for all go-routines finished
 	wg.Wait()
 
-	//Check for error count and return an error if error counter >= M
-	if errCount >= uint32(M) {
+	// Check for error count and return an error if error counter >= M
+	if errCount >= uint32(m) {
 		return ErrErrorsLimitExceeded
 	}
 	return nil
