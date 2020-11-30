7394bf6d4e67803a02d4d8d30864f25a39a92287
Gribkov Ivan <igribkov@corp.badoo.com>
2020-12-01 00:11:47 +0300

    hw 07 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +12,38 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here
+	sourceFile, err := os.Open(fromPath)
+	if err != nil {
+		return ErrUnsupportedFile
+	}
+	defer sourceFile.Close()
+
+	sourceFileInfo, err := sourceFile.Stat()
+	if err != nil {
+		return err
+	}
+
+	if sourceFileInfo.Size() == 0 || sourceFileInfo.IsDir() {
+		return ErrUnsupportedFile
+	} else if offset > sourceFileInfo.Size() {
+		return ErrOffsetExceedsFileSize
+	} else if limit == 0 || limit > sourceFileInfo.Size()-offset {
+		limit = sourceFileInfo.Size() - offset
+	}
+
+	if _, err = sourceFile.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	destinationFile, err := os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer destinationFile.Close()
+
+	if _, err = io.CopyN(destinationFile, sourceFile, limit); err != nil {
+		return err
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,54 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	t.Run("Doesn't exist", func(t *testing.T) {
+		err := Copy("test", "/tmp", 0, 0)
+		require.NotNil(t, err)
+	})
+
+	t.Run("fileFrom is dir", func(t *testing.T) {
+		err := Copy("/tmp", "test", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("fileTo is dir", func(t *testing.T) {
+		_, err := os.Create("test")
+		if err != nil {
+			t.Error(err)
+		}
+		defer os.Remove("test")
+		err = Copy("/tmp", "test", 0, 0)
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("copy empty file", func(t *testing.T) {
+		_, err := os.Create("test")
+		if err != nil {
+			t.Error(err)
+		}
+		defer os.Remove("test")
+		err = Copy("test", "test1", 10, 0)
+
+		require.Equal(t, ErrUnsupportedFile, err)
+	})
+
+	t.Run("offset > file size", func(t *testing.T) {
+		file, err := os.Create("test")
+		if err != nil {
+			t.Error(err)
+		}
+		file.WriteString("test")
+		defer os.Remove("test")
+		err = Copy("test", "test1", 10, 0)
+
+		require.Equal(t, ErrOffsetExceedsFileSize, err)
+	})
+
 }
